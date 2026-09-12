2506c56c2fc21d7dc1a3b652f46213622f9fb8e9
atriaaa <alicee.transfem@gmail.com>
2026-09-12 13:34:03 +0200

    system do wysyłania i generowania biletó

---
+++ wysylanie_biletow/generowanie_qr.py
@@ -0,0 +1,21 @@
+import qrcode
+import hashlib
+
+
+
+def gen_qr(id_biletu):
+    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L,
+                       box_size=10,                
+                       border=4)
+    qr.add_data(id_biletu)
+    qr.make(fit=True)
+
+
+    img = qr.make_image() # fill_color, back_color dla kustomizacji
+    filepath = f"C:/Users/stane_kgevii5/Documents/GitHub/Projekt_Bileciki/wysylanie_biletow/QR-{id_biletu}.png"
+    img.save(filepath)
+
+    return filepath
+
+
+gen_qr('just_testing')
